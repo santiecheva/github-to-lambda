bd60b86e718bc981a556e3acba3a3deb849043d3
Santiago Echeverri <santiecheva@gmail.com>
2021-09-21 11:41:58 -0500

    cd completo

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,5 @@ def lambda_handler(event, context):
    d = {'col1': [1,2], 'col2': [3,4]}
    df = pd.DataFrame(data=d)
    print(df)
-   print('Done x1')
+   print('Done x2')
 
-print(lambda_handler(None,None))
